2649966e2a8263fcdc77f16ca25b45e14f7e743e
TDrachev <drachev.td@gmail.com>
2022-05-19 17:53:12 +0300

    Starter reworking agent to use ARIMA

--- transaction-volume-anomaly-detection/src/agent.config.js
+++ transaction-volume-anomaly-detection/src/agent.config.js
@@ -188,7 +188,7 @@ const contractsARBITRUM = [
 ];
 
 module.exports = {
-  bucketBlockSize: 5,
+  bucketBlockSize: 60,
   globalSensitivity: 1, // Default is 1, Greater than 1 will increase detections, lower than 1 will decrease detections
   getContractsByChainId: (chainId) => {
     switch (chainId) {
--- transaction-volume-anomaly-detection/src/agent.js
+++ transaction-volume-anomaly-detection/src/agent.js
@@ -2,133 +2,248 @@ const {
   Finding,
   FindingSeverity,
   FindingType,
-  getTransactionReceipt,
   getEthersProvider,
+  getTransactionReceipt,
 } = require("forta-agent");
 
 const {
-  bucketBlockSize,
   getContractsByChainId,
   globalSensitivity,
+  bucketBlockSize,
 } = require("./agent.config");
 
-const TimeSeriesAnalysis = require("./TimeSeriesDeviationTracker");
-const contractBuckets = [];
-let contractListLocal = [];
+const ARIMA_SETTINGS = {
+  p: 2,
+  d: 1,
+  q: 2,
+  P: 1,
+  D: 0,
+  Q: 1,
+  s: 168,
+  verbose: false,
+};
+const ARIMA = require("arima");
+const provider = getEthersProvider();
+const contractTracker = {};
+const timestampThreshold = bucketBlockSize * 60 * 60; // The timestamp time in seconds based on the bucket block size
+
+let contractsForChain;
+let isFirstBlock = true;
+let startTimestamp = 0;
+let isTrained = false;
 let isRunningJob = false;
 let localFindings = [];
-const provider = getEthersProvider();
 
-//Here we init the buckets for all contracts that were predefined
+//Initialize the TSA for each contract in the list
 const initialize = async () => {
   const { chainId } = await provider.getNetwork();
-  const contractList = getContractsByChainId(chainId);
+  contractsForChain = getContractsByChainId(chainId);
 
-  contractListLocal = contractList;
-
-  //Here we create the array containing all TimeSeriesAnalysisFields
-  for (let contract of contractListLocal) {
-    const TimeSeriesAnalysisObj = {};
-    TimeSeriesAnalysisObj[contract] = {
-      successfulTx: new TimeSeriesAnalysis(contract, bucketBlockSize),
-      failedTx: new TimeSeriesAnalysis(contract, bucketBlockSize),
-      successfulInternalTx: new TimeSeriesAnalysis(contract, bucketBlockSize),
-      failedInternalTx: new TimeSeriesAnalysis(contract, bucketBlockSize),
+  for (let contract of contractsForChain) {
+    contractTracker[contract] = {
+      successfulTx: {
+        TSA: new ARIMA(ARIMA_SETTINGS),
+        txTracker: [],
+        txCount: 0,
+      },
+      failedTx: {
+        TSA: new ARIMA(ARIMA_SETTINGS),
+        txTracker: [],
+        txCount: 0,
+      },
+      successfulInternalTx: {
+        TSA: new ARIMA(ARIMA_SETTINGS),
+        txTracker: [],
+        txCount: 0,
+      },
+      failedInternalTx: {
+        TSA: new ARIMA(ARIMA_SETTINGS),
+        txTracker: [],
+        txCount: 0,
+      },
     };
-    contractBuckets.push(TimeSeriesAnalysisObj);
   }
 };
 
-//If a transaction occurs in a block that is compatible with our requirements add it to the TSA (TimeSeriesAnalysis)
-function provideHandleTransaction(
-  contractList,
-  contractBuckets,
+//Handle each tx by type and increment if needed
+const provideHandleTransaction = (
+  contractTracker,
+  contractsForChain,
   getTransactionReceipt
-) {
+) => {
   return async function handleTransaction(txEvent) {
     const findings = [];
-    const found = contractList.findIndex((c) => c == txEvent.to);
-    if (found != -1) {
-      const TimeSeriesAnalysisForTx =
-        contractBuckets[found][contractList[found]];
 
-      if (txEvent.traces) {
-        if (txEvent.traces.length > 0) {
-          for (let trace of txEvent.traces) {
-            if (!trace.error) {
-              TimeSeriesAnalysisForTx.successfulInternalTx.addTransaction(
-                txEvent.block.number
-              );
-            } else if (trace.error) {
-              TimeSeriesAnalysisForTx.failedInternalTx.addTransaction(
-                txEvent.block.number
-              );
-            }
+    const contract = contractsForChain.find((c) => c == txEvent.to);
+
+    if (contract) {
+      const tracker = contractTracker[contract];
+
+      if (txEvent.traces.length > 0) {
+        for (const trace of txEvent.traces) {
+          if (!trace.error) {
+            tracker.successfulInternalTx.txCount++;
+          } else if (trace.error) {
+            tracker.failedInternalTx.txCount++;
           }
         }
       } else {
         const { status } = await getTransactionReceipt(txEvent.hash);
-
         if (status) {
-          TimeSeriesAnalysisForTx.successfulTx.addTransaction(
-            txEvent.block.number
-          );
+          tracker.successfulTx.txCount++;
         } else if (!status) {
-          TimeSeriesAnalysisForTx.failedTx.addTransaction(txEvent.block.number);
+          tracker.failedTx.txCount++;
         }
       }
     }
 
     return findings;
   };
-}
+};
 
-//Here we do a check once per block if there is anything unusual per requirements
-function provideHandleBlock(contractBuckets) {
+const provideHandleBlock = (contractTracker, contractsForChain) => {
   return async function handleBlock(blockEvent) {
     let findings = [];
-    if (!isRunningJob) {
-      runJob(blockEvent, contractBuckets);
-    }
 
+    //We skip the first block since it will have no information that we should train the TSA on
+    if (!isFirstBlock) {
+      //On the second block add the txCount to the txTracker array
+      for (let contract of contractsForChain) {
+        const tracker = contractTracker[contract];
+        tracker.successfulTx.txTracker.push(tracker.successfulTx.txCount);
+
+        tracker.failedTx.txTracker.push(tracker.failedTx.txCount);
+        tracker.successfulInternalTx.txTracker.push(
+          tracker.successfulInternalTx.txCount
+        );
+
+        tracker.failedInternalTx.txTracker.push(
+          tracker.failedInternalTx.txCount
+        );
+        tracker.successfulTx.txCount = 0;
+        tracker.failedTx.txCount = 0;
+        tracker.successfulInternalTx.txCount = 0;
+        tracker.failedInternalTx.txCount = 0;
+      }
+
+      //If we pass the timestampThreshold train the SARIMA Model to predict data
+      if (blockEvent.block.timestamp - startTimestamp <= timestampThreshold) {
+        for (let contract of contractsForChain) {
+          const tracker = contractTracker[contract];
+
+          tracker.successfulTx.TSA.train(tracker.successfulTx.txTracker);
+          tracker.failedTx.TSA.train(tracker.failedTx.txTracker);
+          tracker.successfulInternalTx.TSA.train(
+            tracker.successfulInternalTx.txTracker
+          );
+
+          tracker.failedInternalTx.TSA.train(
+            tracker.failedInternalTx.txTracker
+          );
+        }
+        isTrained = true;
+        startTimestamp = blockEvent.block.timestamp;
+      }
+
+      //If the model is trained and we are not running a job, Run job
+      if (!isRunningJob && isTrained) {
+        runJob(contractTracker, contractsForChain, blockEvent);
+        isTrained = false;
+      }
+    } else {
+      startTimestamp = blockEvent.block.timestamp;
+    }
     if (localFindings.length > 0) {
       findings = localFindings;
       localFindings = [];
     }
-
+    isFirstBlock = false;
     return findings;
   };
-}
+};
 
-//Asynchronously check for findings - needed since there is a lot of evaluations happening in the background
-async function runJob(blockEvent, contractBuckets) {
+//Returns findings is there are positive cases
+const runJob = (contractTracker, contractsForChain, blockEvent) => {
   isRunningJob = true;
-  const findings = [];
-  for (let TimeSeriesAnalysisBucket of contractBuckets) {
-    const TimeSeriesAnalysisBuckets = Object.values(TimeSeriesAnalysisBucket);
-    const TimeSeriesAnalysisSuccessfulTx =
-      TimeSeriesAnalysisBuckets[0].successfulTx;
-    const TimeSeriesAnalysisFailedTx = TimeSeriesAnalysisBuckets[0].failedTx;
-    const TimeSeriesAnalysisSuccessfulInternalTx =
-      TimeSeriesAnalysisBuckets[0].successfulInternalTx;
+  isFirstBlock = true;
 
-    const TimeSeriesAnalysisFailedInternalTx =
-      TimeSeriesAnalysisBuckets[0].failedInternalTx;
+  for (let contract of contractsForChain) {
+    const tracker = contractTracker[contract];
 
-    if (TimeSeriesAnalysisSuccessfulInternalTx.isFull()) {
-      const std =
-        TimeSeriesAnalysisSuccessfulInternalTx.getStdForLatestBucket();
+    {
+      const [pred, error] = tracker.successfulTx.TSA.predict(1);
 
-      const normalMargin =
-        TimeSeriesAnalysisSuccessfulInternalTx.getNormalMarginOfDifferences();
+      const count =
+        tracker.successfulTx.txTracker[
+          tracker.successfulTx.txTracker.length - 1
+        ];
 
-      if (Math.floor(std) * globalSensitivity > normalMargin) {
-        const count =
-          TimeSeriesAnalysisSuccessfulInternalTx.getTotalForLastBucket();
+      const baseline =
+        tracker.successfulTx.txTracker[
+          tracker.successfulTx.txTracker.length - 2
+        ];
 
-        const baseline =
-          TimeSeriesAnalysisSuccessfulInternalTx.getBaselineForLastBucket();
-        findings.push(
+      if (count * error[0] * globalSensitivity > pred[0]) {
+        localFindings.push(
+          Finding.fromObject({
+            name: "Unusually high number of successful transactions",
+            description: `Significant increase of successful transactions have been observed from ${
+              blockEvent.blockNumber - bucketBlockSize
+            } to ${blockEvent.blockNumber}`,
+            alertId: "SUCCESSFUL-TRANSACTION-VOL-INCREASE",
+            severity: FindingSeverity.Low,
+            type: FindingType.Suspicious,
+            metadata: {
+              COUNT: count,
+              EXPECTED_BASELINE: baseline,
+            },
+          })
+        );
+      }
+    }
+
+    {
+      const [pred, error] = tracker.failedTx.TSA.predict(1);
+      tracker.failedTx.TSA.predict(1);
+
+      const count =
+        tracker.failedTx.txTracker[tracker.failedTx.txTracker.length - 1];
+      const baseline =
+        tracker.failedTx.txTracker[tracker.failedTx.txTracker.length - 2];
+
+      if (count * error[0] * globalSensitivity > pred[0]) {
+        localFindings.push(
+          Finding.fromObject({
+            name: "Unusually high number of failed transactions",
+            description: `Significant increase of failed transactions have been observed from  ${
+              blockEvent.blockNumber - bucketBlockSize
+            } to ${blockEvent.blockNumber}`,
+            alertId: "FAILED-TRANSACTION-VOL-INCREASE",
+            severity: FindingSeverity.High,
+            type: FindingType.Exploit,
+            metadata: {
+              COUNT: count,
+              EXPECTED_BASELINE: baseline,
+            },
+          })
+        );
+      }
+    }
+
+    {
+      const [pred, error] = tracker.successfulInternalTx.TSA.predict(1);
+
+      const count =
+        tracker.successfulInternalTx.txTracker[
+          tracker.successfulInternalTx.txTracker.length - 1
+        ];
+      const baseline =
+        tracker.successfulInternalTx.txTracker[
+          tracker.successfulInternalTx.txTracker.length - 2
+        ];
+
+      if (count * error[0] * globalSensitivity > pred[0]) {
+        localFindings.push(
           Finding.fromObject({
             name: "Unusually high number of successful internal transactions",
             description: `Significant increase of successful internal transactions have been observed from ${
@@ -146,19 +261,16 @@ async function runJob(blockEvent, contractBuckets) {
       }
     }
 
-    if (TimeSeriesAnalysisFailedInternalTx.isFull()) {
-      const std = TimeSeriesAnalysisFailedInternalTx.getStdForLatestBucket();
+    {
+      const [pred, error] = tracker.failedInternalTx.TSA.predict(1);
+      const count =
+        tracker.failedInternalTx.txTracker[
+          tracker.failedInternalTx.txTracker.length - 2
+        ];
+      const baseline = pred[0];
 
-      const normalMargin =
-        TimeSeriesAnalysisFailedInternalTx.getNormalMarginOfDifferences();
-
-      if (Math.floor(std) * globalSensitivity > normalMargin) {
-        const count =
-          TimeSeriesAnalysisFailedInternalTx.getTotalForLastBucket();
-
-        const baseline =
-          TimeSeriesAnalysisFailedInternalTx.getBaselineForLastBucket();
-        findings.push(
+      if (count * error[0] * globalSensitivity > pred[0]) {
+        localFindings.push(
           Finding.fromObject({
             name: "Unusually high number of failed internal transactions",
             description: `Significant increase of failed internal transactions have been observed from ${
@@ -176,76 +288,24 @@ async function runJob(blockEvent, contractBuckets) {
       }
     }
 
-    if (TimeSeriesAnalysisFailedTx.isFull()) {
-      const std = TimeSeriesAnalysisFailedTx.getStdForLatestBucket();
-
-      const normalMargin =
-        TimeSeriesAnalysisFailedTx.getNormalMarginOfDifferences();
-
-      if (Math.floor(std) * globalSensitivity > normalMargin) {
-        const count = TimeSeriesAnalysisFailedTx.getTotalForLastBucket();
-
-        const baseline = TimeSeriesAnalysisFailedTx.getBaselineForLastBucket();
-        findings.push(
-          Finding.fromObject({
-            name: "Unusually high number of failed transactions",
-            description: `Significant increase of failed transactions have been observed from  ${
-              blockEvent.blockNumber - bucketBlockSize
-            } to ${blockEvent.blockNumber}`,
-            alertId: "FAILED-TRANSACTION-VOL-INCREASE",
-            severity: FindingSeverity.High,
-            type: FindingType.Exploit,
-            metadata: {
-              COUNT: count,
-              EXPECTED_BASELINE: baseline,
-            },
-          })
-        );
-      }
-    }
-    if (TimeSeriesAnalysisSuccessfulTx.isFull()) {
-      const std = TimeSeriesAnalysisSuccessfulTx.getStdForLatestBucket();
-
-      const normalMargin =
-        TimeSeriesAnalysisSuccessfulTx.getNormalMarginOfDifferences();
-
-      if (Math.floor(std) * globalSensitivity > normalMargin) {
-        const count = TimeSeriesAnalysisSuccessfulTx.getTotalForLastBucket();
-
-        const baseline =
-          TimeSeriesAnalysisSuccessfulTx.getBaselineForLastBucket();
-        findings.push(
-          Finding.fromObject({
-            name: "Unusually high number of successful transactions",
-            description: `Significant increase of successful transactions have been observed from ${
-              blockEvent.blockNumber - bucketBlockSize
-            } to ${blockEvent.blockNumber}`,
-            alertId: "SUCCESSFUL-TRANSACTION-VOL-INCREASE",
-            severity: FindingSeverity.Low,
-            type: FindingType.Suspicious,
-            metadata: {
-              COUNT: count,
-              EXPECTED_BASELINE: baseline,
-            },
-          })
-        );
-      }
-    }
+    tracker.successfulTx.txTracker = [];
+    tracker.failedTx.txTracker = [];
+    tracker.successfulInternalTx.txTracker = [];
+    tracker.failedInternalTx.txTracker = [];
   }
 
   isRunningJob = false;
-  localFindings = findings;
-}
+};
 
 module.exports = {
   initialize,
   handleTransaction: provideHandleTransaction(
-    contractListLocal,
-    contractBuckets,
+    contractTracker,
+    contractsForChain,
     getTransactionReceipt
   ),
-  handleBlock: provideHandleBlock(contractBuckets),
+  handleBlock: provideHandleBlock(contractTracker, contractsForChain),
   provideHandleTransaction,
   provideHandleBlock,
-  runJob,
+  resetIsFirstBlock: () => (isFirstBlock = true),
 };
