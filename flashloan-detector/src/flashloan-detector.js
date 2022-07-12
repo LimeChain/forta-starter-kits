d1c2e5d38a0c274ad74d877051e72d5e66790268
Deyan Dimitrov <dikelito@tutamail.com>
2022-07-12 09:17:33 +0300

    Add Uniswap V2 and V3 support to the flashloan detector (#27)

--- flashloan-detector/src/flashloan-detector.js
+++ flashloan-detector/src/flashloan-detector.js
@@ -3,6 +3,8 @@ const { getDydxFlashloan } = require('./detectors/dydx-detector');
 const { getEulerFlashloan } = require('./detectors/euler-detector');
 const { getIronBankFlashloan } = require('./detectors/iron-bank-detector');
 const { getMakerFlashloan } = require('./detectors/maker-detector');
+const { getUniswapV2Flashloan } = require('./detectors/uniswap-v2-detector');
+const { getUniswapV3Flashloan } = require('./detectors/uniswap-v3-detector');
 
 module.exports = {
   // Returns an array of protocols from which a flashloan was taken
@@ -13,6 +15,8 @@ module.exports = {
     const eulerFlashloans = getEulerFlashloan(txEvent);
     const ironBankFlashloans = await getIronBankFlashloan(txEvent);
     const makerFlashloans = getMakerFlashloan(txEvent);
+    const uniswapV2Flashloans = await getUniswapV2Flashloan(txEvent);
+    const uniswapV3Flashloans = await getUniswapV3Flashloan(txEvent);
 
     flashloanProtocols.push(
       ...aaveFlashloans,
@@ -20,6 +24,8 @@ module.exports = {
       ...eulerFlashloans,
       ...ironBankFlashloans,
       ...makerFlashloans,
+      ...uniswapV2Flashloans,
+      ...uniswapV3Flashloans,
     );
 
     return flashloanProtocols;
